9d7eceeebd179b34c6583b5ad2ac1da3ae047898
toma570 <itextoma@gmail.com>
2023-06-29 17:40:34 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import NavBar from "./components/NavBar/NavBar";
 // import ParticlesEffect from './components/Particles/Particles';
 import Portfolio from "./components/Portfolio/Portfolio";
 import Skills from "./components/Skills/Skills";
+import { FaFacebook, FaGithub, FaLinkedin, FaVoicemail } from "react-icons/fa6";
+import { HiMail } from "react-icons/hi";
 
 const App = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
-	console.log(isOpen)
+	console.log(isOpen);
 	return (
 		<div>
 			<div className='w-full'>
@@ -29,6 +31,51 @@ const App = () => {
 					<Portfolio />
 					<Skills />
 					<Contact />
+				</div>
+			</div>
+
+			<div className=' fixed top-1/2 -translate-y-1/2 right-0'>
+				<div className='flex flex-col items-end'>
+					<a href=''>
+						<div className='flex items-center group '>
+							<div className='p-4 bg-primary-color text-white text-2xl group-hover:rounded-tl group-hover:rounded-bl rounded-tl'>
+								<FaLinkedin />
+							</div>
+							<div className='bg-primary-color text-white py-4 max-w-0 group-hover:max-w-[100px] duration-500'>
+								LinkedIn
+							</div>
+						</div>
+					</a>
+					<a href=''>
+						<div className='flex items-center group '>
+							<div className='p-4 bg-primary-color text-white text-2xl group-hover:rounded-tl group-hover:rounded-bl'>
+								<FaGithub />
+							</div>
+							<div className='bg-primary-color text-white py-4 max-w-0 group-hover:max-w-[100px] duration-500'>
+								LinkedIn
+							</div>
+						</div>
+					</a>
+					<a href=''>
+						<div className='flex items-center group '>
+							<div className='p-4 bg-primary-color text-white text-2xl group-hover:rounded-tl group-hover:rounded-bl'>
+								<HiMail />
+							</div>
+							<div className='bg-primary-color text-white py-4 max-w-0 group-hover:max-w-[100px] duration-500'>
+								Mail
+							</div>
+						</div>
+					</a>
+					<a href=''>
+						<div className='flex items-center group '>
+							<div className='p-4 bg-primary-color text-white text-2xl group-hover:rounded-tl group-hover:rounded-bl rounded-bl'>
+								<FaFacebook />
+							</div>
+							<div className='bg-primary-color text-white py-4 max-w-0 group-hover:max-w-[100px] duration-500'>
+								Facebook
+							</div>
+						</div>
+					</a>
 				</div>
 			</div>
 		</div>
